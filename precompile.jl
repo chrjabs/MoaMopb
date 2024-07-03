c63dd6ccca5f859b6f3b0083fb3c2fae064da5b2
Christoph Jabs <christoph.jabs@helsinki.fi>
2024-07-03 11:44:39 +0300

    update precompile script to cover all options

--- precompile.jl
+++ precompile.jl
@@ -3,9 +3,22 @@ MoaMopb.main()
 import HiGHS
 import MultiObjectiveAlgorithms as MOA
 MoaMopb.run("tiny-inst.mopb", HiGHS.Optimizer, MOA.Lexicographic(), nothing, true, true, false)
+MoaMopb.run("tiny-inst.mopb", HiGHS.Optimizer, MOA.EpsilonConstraint(), nothing, true, true, true)
+MoaMopb.run("tiny-inst.mopb", HiGHS.Optimizer, MOA.Hierarchical(), nothing, true, true, true)
+MoaMopb.run("tiny-inst.mopb", HiGHS.Optimizer, MOA.TambyVanderpooten(), nothing, true, true, false)
+MoaMopb.run("tiny-inst.mopb", HiGHS.Optimizer, MOA.KirlikSayin(), nothing, true, true, false)
+MoaMopb.run("tiny-inst.mopb", HiGHS.Optimizer, MOA.DominguezRios(), nothing, true, true, false)
 import SCIP
-import MultiObjectiveAlgorithms as MOA
+MoaMopb.run("tiny-inst.mopb", SCIP.Optimizer, MOA.Lexicographic(), nothing, true, true, true)
 MoaMopb.run("tiny-inst.mopb", SCIP.Optimizer, MOA.EpsilonConstraint(), nothing, true, true, true)
+MoaMopb.run("tiny-inst.mopb", SCIP.Optimizer, MOA.Hierarchical(), nothing, true, true, true)
+MoaMopb.run("tiny-inst.mopb", SCIP.Optimizer, MOA.TambyVanderpooten(), nothing, true, true, false)
+MoaMopb.run("tiny-inst.mopb", SCIP.Optimizer, MOA.KirlikSayin(), nothing, true, true, false)
+MoaMopb.run("tiny-inst.mopb", SCIP.Optimizer, MOA.DominguezRios(), nothing, true, true, false)
 import Gurobi
-import MultiObjectiveAlgorithms as MOA
+MoaMopb.run("tiny-inst.mopb", Gurobi.Optimizer, MOA.Lexicographic(), nothing, true, true, true)
+MoaMopb.run("tiny-inst.mopb", Gurobi.Optimizer, MOA.EpsilonConstraint(), nothing, true, true, true)
 MoaMopb.run("tiny-inst.mopb", Gurobi.Optimizer, MOA.Hierarchical(), nothing, true, true, true)
+MoaMopb.run("tiny-inst.mopb", Gurobi.Optimizer, MOA.TambyVanderpooten(), nothing, true, true, false)
+MoaMopb.run("tiny-inst.mopb", Gurobi.Optimizer, MOA.KirlikSayin(), nothing, true, true, false)
+MoaMopb.run("tiny-inst.mopb", Gurobi.Optimizer, MOA.DominguezRios(), nothing, true, true, false)
